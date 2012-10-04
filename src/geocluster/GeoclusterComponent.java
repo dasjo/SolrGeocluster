6acf47fa7b4ddc358cc3f100d34eec9fec1793cf
dasjo <dasjogibtsnet@gmail.com>
2012-10-04 11:43:18 +0200

    Cleanup and remove dependency on Solr's Clustering Component.

--- src/geocluster/GeoclusterComponent.java
+++ src/geocluster/GeoclusterComponent.java
@@ -17,23 +17,14 @@ package geocluster;
  */
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.apache.solr.common.SolrDocument;
-import org.apache.solr.common.SolrDocumentList;
-import org.apache.solr.common.params.CommonParams;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.core.SolrCore;
-import org.apache.solr.core.SolrResourceLoader;
-import org.apache.solr.handler.clustering.ClusteringEngine;
 import org.apache.solr.handler.clustering.ClusteringParams;
-import org.apache.solr.handler.clustering.SearchClusteringEngine;
-import org.apache.solr.handler.clustering.carrot2.CarrotClusteringEngine;
 import org.apache.solr.handler.component.ResponseBuilder;
 import org.apache.solr.handler.component.SearchComponent;
 import org.apache.solr.handler.component.ShardRequest;
@@ -44,23 +35,11 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * Provide a plugin for clustering results.  Can either be for search results (i.e. via Carrot2) or for
- * clustering documents (i.e. via Mahout)
- * <p/>
- * This engine is experimental.  Output from this engine is subject to change in future releases.
- *
- * <pre class="prettyprint" >
- * &lt;searchComponent class="org.apache.solr.handler.clustering.ClusteringComponent" name="clustering"&gt;
- *   &lt;lst name="engine"&gt;
- *     &lt;str name="name"&gt;default&lt;/str&gt;
- *     &lt;str name="carrot.algorithm"&gt;org.carrot2.clustering.lingo.LingoClusteringAlgorithm&lt;/str&gt;
- *   &lt;/lst&gt;
- * &lt;/searchComponent&gt;</pre>
+ * Provide a plugin for geo-clustering results.  
  */
 public class GeoclusterComponent extends SearchComponent implements SolrCoreAware {
   private transient static Logger log = LoggerFactory.getLogger(GeoclusterComponent.class);
 
-  private Map<String, SearchClusteringEngine> searchClusteringEngines = new HashMap<String, SearchClusteringEngine>();
   /**
    * Base name for all spell checker query parameters. This name is also used to
    * register this component with SearchHandler.
@@ -82,26 +61,12 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
     if (!params.getBool(COMPONENT_NAME, false)) {
       return;
     }
-    String name = getClusteringEngineName(rb);
-    SearchClusteringEngine engine = getSearchClusteringEngine(rb);
-    if (engine != null) {
-      DocListAndSet results = rb.getResults();
-      Map<SolrDocument,Integer> docIds = new HashMap<SolrDocument, Integer>(results.docList.size());
-      SolrDocumentList solrDocList = engine.getSolrDocumentList(results.docList, rb.req, docIds);
-      Object clusters = engine.cluster(rb.getQuery(), solrDocList, docIds, rb.req);
-      rb.rsp.add("clusters", clusters);
-    } else {
-      log.warn("No engine for: " + name);
-    }
+    DocListAndSet results = rb.getResults();
+    Map<SolrDocument,Integer> docIds = new HashMap<SolrDocument, Integer>(results.docList.size());
+    // Object clusters = engine.cluster(rb.getQuery(), solrDocList, docIds, rb.req);
+    // rb.rsp.add("clusters", clusters);
   }
   
-  private SearchClusteringEngine getSearchClusteringEngine(ResponseBuilder rb){
-    return searchClusteringEngines.get(getClusteringEngineName(rb));
-  }
-  
-  private String getClusteringEngineName(ResponseBuilder rb){
-    return rb.req.getParams().get(ClusteringParams.ENGINE_NAME, ClusteringEngine.DEFAULT_ENGINE_NAME);
-  }
 
   @Override
   public void modifyRequest(ResponseBuilder rb, SearchComponent who, ShardRequest sreq) {
@@ -110,55 +75,6 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
       return;
     }
     sreq.params.remove(COMPONENT_NAME);
-    if( ( sreq.purpose & ShardRequest.PURPOSE_GET_FIELDS ) != 0 ){
-      String fl = sreq.params.get(CommonParams.FL,"*");
-      // if fl=* then we don't need check
-      if( fl.indexOf( '*' ) >= 0 ) return;
-      Set<String> fields = null; //getSearchClusteringEngine(rb).getFieldsToLoad(rb.req);
-      if( fields == null || fields.size() == 0 ) return;
-      StringBuilder sb = new StringBuilder();
-      String[] flparams = fl.split( "[,\\s]+" );
-      Set<String> flParamSet = new HashSet<String>(flparams.length);
-      for( String flparam : flparams ){
-        // no need trim() because of split() by \s+
-        flParamSet.add(flparam);
-      }
-      for( String aFieldToLoad : fields ){
-        if( !flParamSet.contains( aFieldToLoad ) ){
-          sb.append( ',' ).append( aFieldToLoad );
-        }
-      }
-      if( sb.length() > 0 ){
-        sreq.params.set( CommonParams.FL, fl + sb.toString() );
-      }
-    }
-  }
-
-  @Override
-  public void finishStage(ResponseBuilder rb) {
-    SolrParams params = rb.req.getParams();
-    if (!params.getBool(COMPONENT_NAME, false) || !params.getBool(ClusteringParams.USE_SEARCH_RESULTS, false)) {
-      return;
-    }
-    if (rb.stage == ResponseBuilder.STAGE_GET_FIELDS) {
-      SearchClusteringEngine engine = getSearchClusteringEngine(rb);
-      if (engine != null) {
-        SolrDocumentList solrDocList = (SolrDocumentList)rb.rsp.getValues().get("response");
-        // TODO: Currently, docIds is set to null in distributed environment.
-        // This causes CarrotParams.PRODUCE_SUMMARY doesn't work.
-        // To work CarrotParams.PRODUCE_SUMMARY under distributed mode, we can choose either one of:
-        // (a) In each shard, ClusteringComponent produces summary and finishStage()
-        //     merges these summaries.
-        // (b) Adding doHighlighting(SolrDocumentList, ...) method to SolrHighlighter and
-        //     making SolrHighlighter uses "external text" rather than stored values to produce snippets.
-        Map<SolrDocument,Integer> docIds = null;
-        Object clusters = engine.cluster(rb.getQuery(), solrDocList, docIds, rb.req);
-        rb.rsp.add("clusters", clusters);
-      } else {
-        String name = getClusteringEngineName(rb);
-        log.warn("No engine for: " + name);
-      }
-    }
   }
 
   @Override
@@ -169,52 +85,6 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
   }
 
   public void inform(SolrCore core) {
-    if (initParams != null) {
-      log.info("Initializing Clustering Engines");
-      boolean searchHasDefault = false;
-      boolean documentHasDefault = false;
-      for (int i = 0; i < initParams.size(); i++) {
-        if (initParams.getName(i).equals("engine")) {
-          NamedList engineNL = (NamedList) initParams.getVal(i);
-          String className = (String) engineNL.get("classname");
-          if (className == null) {
-            className = GeoclusterEngine.class.getName();
-          }
-          SolrResourceLoader loader = core.getResourceLoader();
-          ClusteringEngine clusterer = (ClusteringEngine) loader.newInstance(className);
-          if (clusterer != null) {
-            String name = clusterer.init(engineNL, core);
-            if (name != null) {
-              boolean isDefault = name.equals(ClusteringEngine.DEFAULT_ENGINE_NAME);
-              if (clusterer instanceof SearchClusteringEngine) {
-                if (isDefault == true && searchHasDefault == false) {
-                  searchHasDefault = true;
-                } else if (isDefault == true && searchHasDefault == true) {
-                  throw new RuntimeException("More than one engine is missing name: " + engineNL);
-                }
-                searchClusteringEngines.put(name, (SearchClusteringEngine) clusterer);
-              } 
-            } else {
-              if (clusterer instanceof SearchClusteringEngine && searchHasDefault == false) {
-                searchClusteringEngines.put(ClusteringEngine.DEFAULT_ENGINE_NAME, (SearchClusteringEngine) clusterer);
-                searchHasDefault = true;
-              } 
-              else {
-                throw new RuntimeException("More than one engine is missing name: " + engineNL);
-              }
-            }
-          }
-        }
-      }
-      log.info("Finished Initializing Clustering Engines");
-    }
-  }
-
-  /*
-  * @return Unmodifiable Map of the engines, key is the name from the config, value is the engine
-  * */
-  public Map<String, SearchClusteringEngine> getSearchClusteringEngines() {
-    return Collections.unmodifiableMap(searchClusteringEngines);
   }
 
   // ///////////////////////////////////////////
