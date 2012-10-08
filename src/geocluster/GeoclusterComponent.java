a18601af3b6a8eb1adb1c656946c79a77edb22b4
dasjo <dasjogibtsnet@gmail.com>
2012-10-08 14:28:25 +0200

    Update logic to read from grouped results.

--- src/geocluster/GeoclusterComponent.java
+++ src/geocluster/GeoclusterComponent.java
@@ -17,12 +17,15 @@ package geocluster;
  */
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 
 import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.params.SolrParams;
 import org.apache.solr.common.util.NamedList;
 import org.apache.solr.core.SolrCore;
@@ -30,7 +33,13 @@ import org.apache.solr.handler.clustering.ClusteringParams;
 import org.apache.solr.handler.component.ResponseBuilder;
 import org.apache.solr.handler.component.SearchComponent;
 import org.apache.solr.handler.component.ShardRequest;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.search.DocIterator;
+import org.apache.solr.search.DocList;
 import org.apache.solr.search.DocListAndSet;
+import org.apache.solr.search.DocSlice;
+import org.apache.solr.search.SolrIndexSearcher;
+import org.apache.solr.util.SolrPluginUtils;
 import org.apache.solr.util.plugin.SolrCoreAware;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,12 +57,15 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
    */
   public static final String COMPONENT_NAME = "geocluster";
   private NamedList initParams;
-
+  
   @Override
   public void prepare(ResponseBuilder rb) throws IOException {
     SolrParams params = rb.req.getParams();
-    if (!params.getBool(COMPONENT_NAME, false)) {
-      return;
+    if (params.getBool(COMPONENT_NAME, false)) {
+      // TODO: this just doesn't work
+      rb.setNeedDocList( true );
+      // Alternative workaround, see getDocList in Grouping.java
+      rb.setFieldFlags(SolrIndexSearcher.GET_DOCLIST);
     }
   }
 
@@ -65,7 +77,37 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
     }
     DocListAndSet results = rb.getResults();
     Map<SolrDocument,Integer> docIds = new HashMap<SolrDocument, Integer>(results.docList.size());
+    SolrDocumentList solrDocList = getSolrDocumentList(results.docList, rb.req, docIds);
     
+    String field = "f_ss_field_place:geohash_geocluster_index_3";
+    
+
+    NamedList values = rb.rsp.getValues();
+    NamedList grouped = (NamedList)values.get("grouped");
+    NamedList groupedValue = (NamedList)grouped.get(field);
+    ArrayList<NamedList> groups = (ArrayList)groupedValue.get("groups");
+
+    if (groups != null) {
+      for (NamedList group : groups) {
+        String geohashPrefix = (String)group.get("groupValue");
+        log.info("Prefix: " + geohashPrefix);
+
+        DocSlice docList = (DocSlice)group.get("doclist");
+        DocIterator iterator = docList.iterator();
+        while (iterator.hasNext()) {
+          Integer docId = iterator.next();
+          SolrDocument doc = solrDocList.get(docId);
+          String geohash = (String)doc.getFieldValue("ss_field_place:geohash");
+          String latlon = (String)doc.getFieldValue("t_field_place:latlon");
+          String id = (String)doc.getFieldValue("ss_search_api_id");
+          
+          log.info("Doc: " + id + ", geohash: " + geohash + ", latlon: " + latlon);
+        }
+        
+      }
+    }
+    
+    /*
     String field = "f_sm_field_place:geohash_geocluster_index";
 
     NamedList values = rb.rsp.getValues();
@@ -81,9 +123,27 @@ public class GeoclusterComponent extends SearchComponent implements SolrCoreAwar
         Integer count = entry.getValue();
       }
     }
+    */
     
     // Object clusters = engine.cluster(rb.getQuery(), solrDocList, docIds, rb.req);
     // rb.rsp.add("clusters", clusters);
+  }
+  
+  /**
+   * Returns the set of field names to load.
+   * Concrete classes can override this method if needed.
+   * Default implementation returns null, that is, all stored fields are loaded.
+   * @param sreq
+   * @return set of field names to load
+   */
+  protected Set<String> getFieldsToLoad(SolrQueryRequest sreq){
+    return null;
+  }
+
+  protected SolrDocumentList getSolrDocumentList(DocList docList, SolrQueryRequest sreq,
+      Map<SolrDocument, Integer> docIds) throws IOException{
+    return SolrPluginUtils.docListToSolrDocumentList(
+        docList, sreq.getSearcher(), getFieldsToLoad(sreq), docIds);
   }
   
 
